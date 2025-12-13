43e9f60c3460ddd02815371f5d7b0e501f64db5a
Luki Riski <contactlukiriski@gmail.com>
2025-12-13 10:58:49 +0700

    feat(main): initialize UserModule and AuthModule with global routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lukiriskigumilar/resepify-be/internal/auth"
+	"github.com/lukiriskigumilar/resepify-be/internal/users"
+
+	"github.com/lukiriskigumilar/resepify-be/internal/config"
+	"github.com/lukiriskigumilar/resepify-be/internal/routes"
 )
 
 func main() {
+	config.ConnectDatabase()
+	db := config.DB
+
+	//init modules
+	usersModule := users.InitUserModule(db)
+	authModule := auth.InitAuthModule(usersModule)
+
 	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	//init routes
+	routes.GlobalRoutes(api, authModule)
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
