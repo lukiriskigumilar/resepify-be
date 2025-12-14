994f585a4aa59c2d4b73b4634fa2495b2c5007b6
Luki Riski <contactlukiriski@gmail.com>
2025-12-14 18:30:42 +0700

    feat(auth): add login service

--- internal/auth/services.go
+++ internal/auth/services.go
@@ -2,7 +2,10 @@ package auth
 
 import (
 	"errors"
+	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/lukiriskigumilar/resepify-be/internal/users"
 	"golang.org/x/crypto/bcrypt"
@@ -10,6 +13,7 @@ import (
 
 type AuthService interface {
 	RegisterService(input RegisterRequestDTO) (*users.User, error)
+	LoginService(input LoginRequestDTO) (*LoginResponseDTO, error)
 }
 
 type authService struct {
@@ -47,4 +51,40 @@ func (s *authService) RegisterService(input RegisterRequestDTO) (*users.User, er
 		return nil, err
 	}
 	return newUser, nil
+}
+
+func (s *authService) LoginService(input LoginRequestDTO) (*LoginResponseDTO, error) {
+
+	user, err := s.repo.FindByEmail(input.Email)
+	if err != nil {
+		return nil, errors.New("invalid email or password")
+	}
+
+	//compare password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
+	if err != nil {
+		return nil, errors.New("invalid email or password")
+	}
+
+	ProcessesToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"email":   user.Email,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	dsn := os.Getenv("JWT_SECRET")
+
+	token, err := ProcessesToken.SignedString([]byte(dsn))
+	if err != nil {
+		return nil, err
+	}
+
+	responseData := &LoginResponseDTO{
+		Name:        user.Name,
+		Email:       user.Email,
+		Token:       token,
+		TokenExpiry: time.Now().Add(time.Hour * 24),
+	}
+
+	return responseData, nil
 }
